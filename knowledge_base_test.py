4c2c42709030f785ecffb10234cc63a0e58bea3c
Yiding Tian <jaytin1218@gmail.com>
2014-07-23 11:48:19 +0800

    更新answer cover
    2014.07.23 11:48

--- knowledge_base_test.py
+++ knowledge_base_test.py
@@ -20,6 +20,7 @@ class WordlistTestThread(Thd):
 #答案数据的覆盖度
 	total_answer = 0
 	answer_cover = 0
+	total_word = 0
 #clue整体与clue的部分（keyword等snippets）的查询结果
 	full_clue_matched = 0
 	part_clue_matched = 0
@@ -27,35 +28,53 @@ class WordlistTestThread(Thd):
 	def __init__(self, clue, answer):
 		super(WordlistTestThread, self).__init__(name = answer)
 		self._clue = clue
-		remove_symbles.sub('', self._clue)
+		self._whole_keyword = remove_symbles.sub('', clue)
+		self._part_keywords = 
 		self._answer = answer
 		self._query_url = 'http://en.wordlist.eu/search/phrase,'
 		self._words_url = 'http://en.wordlist.eu/words/letter,'
 		self.__class__.total_answer += 1
 
-	def _answer_cover_helper(self, base_url, page):
-		url = base_url + str(page * 100)
-		try:
-			pq(url)
-		except Exception, e:
-			return None
-		word_list = page('div.content li a')
-		for word in word_list:
-			if p(word).text().upper() == self._answer:
-				self.__class__.answer_cover += 1
+	# def _answer_cover_helper(self, base_url, page):
+	# 	url = base_url + str(page * 100)
+	# 	try:
+	# 		pq(url)
+	# 	except Exception, e:
+	# 		return None
+	# 	word_list = page('div.content li a')
+	# 	for word in word_list:
+	# 		if p(word).text().upper() == self._answer:
+	# 			self.__class__.answer_cover += 1
 
 	def _answer_cover(self):
-		first_letter = self._answer[0]
-		base_url = self._words_url + first_letter + '/'
+		woed_number = 0
+		flag = True
+		while flag:
+			base_url = self._query_url + self._answer + '/' + str(word_number)
+			try:
+				page = pq(base_url)
+			except Exception, e:
+				print self._answer + ' error'
+				return None
 
-		try:
-			page = pq(base_url)
-			maxpage_href = pq(page('div.content p.pagination a')[-1]).attr('href')
-			maxpage = int(get_number.search(maxpage_href).group())/100
-		except Exception, e:
-			return None
-		for i in rang (0, maxpage + 1):
-			Thd(target = _answer_cover_helper, args = (base_url, i), name = self._answer + str(i)).start()
+			word_list = page('div.content div.result h2 a')
+			for word in word_list:
+				href = pq(word).attr('href')
+				text = pq(word).text().upper()
+				if text == self._answer:
+					self.__class__.answer_cover += 1
+					flag = False
+					answer_rank = word_number + word_list.index(word)
+					self.__class__.total_word += answer_rank
+					break
+			word_number += 40
+		# first_letter = self._answer[0]
+
+		# try:
+		# except Exception, e:
+		# 	return None
+		# for i in rang (0, maxpage + 1):
+		# 	Thd(target = _answer_cover_helper, args = (base_url, i), name = self._answer + str(i)).start()
 
 	def _clue_cover_helper(self):
 
